dbc0bcb13796b973cb90d98146c37d16e27b6f0b
Nishat-Rahman-web <nishatrahman.webdeveloper@gmail.com>
2024-05-21 03:59:49 -0700

    first commit

---
+++ final project/js/script.js
@@ -0,0 +1,9 @@
+let main_page                 = document.querySelector('#main_page')
+let main_btn                  = document.querySelector('.main_btn')
+let player_one_intro          = document.querySelector('#player_one_intro')
+
+
+// main_btn.addEventListener('click' , ()=>{
+//   main_page.style = "display:none;"
+//   player_one_intro.style = "display:block;"
+// })
